ed4761d3a22f1006880cb7a2553d7381257f9cfa
Lakpa Dietz <ldietz086@gmail.com>
2026-09-23 13:32:40 -0700

    Add functions to shuffle and deal cards

--- lesson_3/twenty_one.py
+++ lesson_3/twenty_one.py
@@ -1,3 +1,5 @@
+import random
+
 SUITS = ['♣', '♦', '♥', '♠']
 RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
@@ -5,3 +7,17 @@ DECK = []
 for suit in SUITS:
     for rank in RANKS:
         DECK.append([suit, rank])
+
+def prompt(message):
+    print(f"=> {message}")
+
+def shuffle(deck):
+    random.shuffle(deck)
+
+def deal_cards(deck):
+    shuffle(deck)
+
+    player_cards = [deck.pop(), deck.pop()]
+    dealer_cards = [deck.pop(), deck.pop()]
+
+    return player_cards, dealer_cards
